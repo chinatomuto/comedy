f2e8d02f0a867175cb15a30dceaef48fcd681e7d
chinatomuto <78049185+chinatomuto@users.noreply.github.com>
2022-04-14 20:53:47 +0100

    Initial Commit

---
+++ comedy/comedyApp.swift
@@ -0,0 +1,17 @@
+//
+//  comedyApp.swift
+//  comedy
+//
+//  Created by nato on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct comedyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
